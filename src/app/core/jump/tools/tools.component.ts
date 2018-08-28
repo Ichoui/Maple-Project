0250e420e57b46689a2c1b0bfdd2a949aaf64f0c
Ichoui <nicol.morgan@outlook.fr>
2018-08-28 19:04:35 +0200

    Tools Jump :)

--- src/app/core/jump/tools/tools.component.ts
+++ src/app/core/jump/tools/tools.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { AngularFirestore } from 'angularfire2/firestore';
 import * as firebase from 'firebase';
+import {JumpService} from "../../../providers/jump/jump.service";
+import {Jump} from "../../../providers/jump/jump";
 
 
 @Component({
@@ -13,20 +15,25 @@ import * as firebase from 'firebase';
 export class ToolsComponent implements OnInit {
 
   public jump$: Observable<{}>;
-  public array = {};
+  public test: any;
+  public jump: Observable<Jump[]>;
 
-  public item: any;
-
-  constructor(public db: AngularFirestore) {
-    this.jump$ = db.collection('jump').valueChanges();
+  constructor(public afs: AngularFirestore, private jumpService: JumpService) {
+    this.jump$ = afs.collection('jump').valueChanges();
   }
 
 
   ngOnInit() {
     this.jump$.subscribe(i => {
-      console.log(i);
-      this.item = i;
+       this.test = i;
+      console.log(this.test);
     })
+
+    // this.jumpService.getItems().subscribe(i => {
+    //   console.log(i);
+    //   this.jump = i;
+    // });
+
   }
 
 }
--- src/app/providers/jump/jump.service.ts
+++ src/app/providers/jump/jump.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Jump } from './jump';
+import {Observable} from "rxjs/index";
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,19 @@ import { Jump } from './jump';
 export class JumpService {
 
   jumpCollec: AngularFirestoreCollection<Jump>;
+  jumps: Observable<Jump[]>;
 
   constructor(public afs: AngularFirestore) {
+    //TODO Le problème vient de cette assignation ... du coup on ne peut subscribe dans tools.component
+    // this.jumps = this.afs.collection('testee').valueChanges();
+
   }
+
+  getItems() {
+    return this.jumps;
+  }
+
+
 }
 
 
